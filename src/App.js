5178db5d887dd1a757b16d2d786653d1a98d895f
venushaamarathunga <avenusha95@gmail.com>
2023-12-28 22:45:03 +0530

    add create.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Create from "./components/create";
+import Read from "./components/read";
+import Update from "./components/update";
 
 function App() {
   return (
-    <div className="main">
-      <h2 className="main-header">React CRUD Operation!</h2>
-    </div>
+    <Router>
+      <div className="main">
+        <h2 className="main-header">React Crud Operations</h2>
+        <div>
+          <Routes>
+            <Route exact path="/create" element={<Create />} />
+          </Routes>
+        </div>
+        <div style={{ marginTop: 20 }}>
+          <Routes>
+            <Route exact path="/read" element={<Read />} />
+          </Routes>
+        </div>
+        <Routes>
+          <Route path="/update" element={<Update />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
